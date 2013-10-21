ba4c7c9adeec62b8cd23e48d10fae6fb3af25260
Alejandro Cornejo <alejandro.cornejo@splendia.com>
2013-10-21 16:57:20 +0200

    formatting the split queries

--- src/geonames_split_tables.sql
+++ src/geonames_split_tables.sql
@@ -1,16 +1,16 @@
 
 CREATE TABLE IF NOT EXISTS geo_city (
-    geonameid int PRIMARY KEY,
-    name varchar(200),
-    latitude decimal(10,7),
-    longitude decimal(10,7),
-    country varchar(2),
-    admin1 varchar(20),
-    admin2 varchar(80),
-    admin3 varchar(20),
-    admin4 varchar(20),
-    population int,
-    timezone varchar(40)
+    geonameid   int PRIMARY KEY,
+    name        varchar(200),
+    latitude    decimal(10,7),
+    longitude   decimal(10,7),
+    country     varchar(2),
+    admin1      varchar(20),
+    admin2      varchar(80),
+    admin3      varchar(20),
+    admin4      varchar(20),
+    population  int,
+    timezone    varchar(40)
 ) CHARACTER SET utf8;
 
 INSERT INTO geo_city
@@ -32,17 +32,18 @@ WHERE
     g.fclass =  'P'
     AND g.fcode = 'PPL';
 
+
 # Get administration 1 LEVEL FROM Spain
 /*
-SELECT gan . alternateName, gg .*
+SELECT gan.alternate_name, gg.*
 FROM geo_geoname AS gg
     JOIN `geo_hierarchy` AS gh
-            ON gg . geonameid = gh . childId
-        JOIN `geo_alternatename` AS gan
-            ON gg . geonameid = gan . geonameid
-               AND gan . isoLanguage = 'es'
-                   AND gan . isShortName = 1
-WHERE gh . parentid = 2510769
-      AND gh . type = 'ADM'
-ORDER BY gan . alternateName ASC
+        ON gg.geoname_id = gh.child_id
+    JOIN `geo_alternatename` AS gan
+        ON gg.geoname_id = gan.geoname_id
+        AND gan.iso_language = 'es'
+        AND gan.is_short = 1
+WHERE gh.parentid = 2510769
+    AND gh.type = 'ADM'
+ORDER BY gan.alternateName ASC
 */
